70ecb54a066529c386711d5561a6f7bad616309a
LakshinaAlexandra <32337786+LakshinaAlexandra@users.noreply.github.com>
2023-09-13 18:04:06 +0300

    Added check of Junit5 full example code

--- src/test/java/SelenideRepositorySearch.java
+++ src/test/java/SelenideRepositorySearch.java
@@ -19,6 +19,19 @@ public class SelenideRepositorySearch {
 
         $$(".markdown-body h4").filterBy(text("Using JUnit5 extend test class:")).shouldHave(size(1));
         $$(".markdown-body h4").filterBy(text("Using JUnit5 extend test class:")).
-                first().sibling(0).$("pre").shouldHave(text("Test"));
+                first().sibling(0).$("pre").shouldHave(text(
+                """
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                                                
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }
+                        """));
     }
 }
